f097e2649bbb0ddb6fc82fc9d9ad06235087c824
Aurelien Vallee <vallee.aurelien@gmail.com>
2013-05-03 21:28:22 +0200

    Minor changes in the layout of the 2d text test

--- tests/test_drawtext.cpp
+++ tests/test_drawtext.cpp
@@ -3,20 +3,71 @@
 
 #include <crashburn/crashburn.h>
 #include <crashburn/engine/engine.h>
-#include <crashburn/scene/intro.h>
+#include <crashburn/scene/scene.h>
+#include <crashburn/engine/2d_text.h>
+
+class MyTestScene : public crashburn::Scene
+{
+public:
+    MyTestScene(crashburn::Engine& engine)
+        : crashburn::Scene(engine)
+    {
+    }
+
+public:
+    virtual bool setup()
+    {
+        // Clear values
+        glClearColor(0.25, 0.5, 0.75, 0);
+        glClearDepth(0);
+
+        // Setup projection matrix
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        glOrtho (0.0, 1024, 0.0, 768, -1.0, 1.0);
+
+        // Setup modelview matrix
+        glMatrixMode(GL_MODELVIEW);
+
+        // Setup the text properties
+        text_.set_text("HELLO");
+        text_.set_color(1., 0., 0.);
+
+        return true;
+    }
+
+    virtual bool cleanup()
+    {
+        return true;
+    }
+
+    virtual void handle_events()
+    {
+        // Exit when we press Escape
+        if (glfwGetKey(GLFW_KEY_ESC)==GLFW_PRESS)
+            engine_.stop();
+    }
+
+    virtual void update(double time_delta)
+    {
+    }
+
+    virtual void render()
+    {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        text_.render();
+    }
+
+private:
+    crashburn::Text2DItem text_;
+};
 
 int main(int argc, char** argv)
 {
     crashburn::Engine& engine = crashburn::Engine::instance();
 
-    engine.set_scene(new crashburn::Intro(engine));
-
-    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-        
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glOrtho (0.0, 1024, 0.0, 768, -1.0, 1.0);
-    glMatrixMode(GL_MODELVIEW);
+    engine.set_scene(new MyTestScene(engine));
 	
     engine.start();
 
